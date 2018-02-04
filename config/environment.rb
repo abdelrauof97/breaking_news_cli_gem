fee1cf40d1979dbf4aaf6a8088bcc31189e8da35
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-02-04 22:39:16 +0000

    a commit

--- config/environment.rb
+++ config/environment.rb
@@ -1,2 +1,4 @@
+
+
 require_relative "../lib/data_scraper"
 #require_relative "../bin"
--- lib/data_scraper.rb
+++ lib/data_scraper.rb
@@ -1,7 +1,15 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
 class Scraper
 
   def self.get_data(profile_link)
-
+    scraper = new
+    doc = Nokogiri::HTML(open(profile_link))
+    binding.pry
   end
 
 end
+
+# ruby lib/data_scraper.rb
