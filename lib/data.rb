53f0ea84e00e396515814f78a6ab9a6b653c9fdc
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-02-05 03:30:54 +0000

    commit

--- lib/data.rb
+++ lib/data.rb
@@ -1,19 +1,20 @@
 
 class Data
 
-  def initialize(url)
-    @news_array = Scraper.get_and_sort_data(url)
-    exposing_news
+  def self.get_data(url)
+    Scraper.get_and_sort_data(url)
   end
 
-  def exposing_news
-    news_array.each.with_index(1) { |x,i|
-      hash = x[i]
-      puts "----------------"
-      puts "#{i}. #{hash[:title]}"
-      puts "ptess the link to see the news: #{hash[:link]}"
-      puts "----------------"
+  def exposing_news(url)
+    new_array =
+    get_data.each.with_index(1) { |x,i|
+      if i < 10
+        hash = x[i]
+        puts "----------------"
+        puts "#{i}. #{hash[:title]}
+        puts "ptess the link to see the news: #{hash[:link]}"
+        puts "----------------"
+      end
     }
   end
-
 end
--- spec/data_spec.rb
+++ spec/data_spec.rb
@@ -1,2 +1,12 @@
 
 require 'spec_helper'
+
+describe "Data class" do
+  url = 'http://www.newsnow.co.uk/h/World+News/Africa/Egypt'
+  context "Data is a class metiod" do
+    it "is a metiod" do
+      Data.exposing_news(url)
+    end
+  end
+
+end
